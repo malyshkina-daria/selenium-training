83c8d760cfbeccee99e66a4160f867c67282357f
Daria <malyshkina.da95@gmail.com>
2017-08-06 22:25:51 +0300

    add exercise9Test

--- src/test/java/ru/stqa/training/selenium/AdminPage.java
+++ src/test/java/ru/stqa/training/selenium/AdminPage.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AdminPage extends LoginAdminPageTest {
@@ -23,5 +25,59 @@ public class AdminPage extends LoginAdminPageTest {
                 }
             }
         }
+    }
+
+    @Test
+    public void exercise9Test() {
+        clickOnMenuByText("Countries");
+        Assert.assertTrue(countriesIsSortedOnCountryPage());
+        clickOnMenuByText("Geo Zones");
+        countriesIsSortedOnGeoZonesPage();
+    }
+
+    private boolean countriesIsSortedOnCountryPage() {
+        List<String> countriesList = new ArrayList<>();
+        List<WebElement> countriesWebElementList = driver.findElements(By.xpath("//tr[@class='row']"));
+        for (int i = 0; i < countriesWebElementList.size(); i++) {
+            WebElement row = driver.findElement(By.xpath("//tr[@class='row'][" + (i + 1) + "]"));
+            WebElement currentElement = row.findElement(By.xpath(".//td[5]/a"));
+            countriesList.add(currentElement.getText());
+            if (Integer.parseInt(row.findElement(By.xpath(".//td[6]")).getText()) != 0) {
+                currentElement.click();
+                List<String> zonesList = new ArrayList<>();
+                List<WebElement> zonesWebElement = driver.findElements(By.xpath("//td[3]/input[starts-with(@name,'zone')]"));
+                for (WebElement webElementZone : zonesWebElement) {
+                    zonesList.add(webElementZone.getText());
+                }
+                Assert.assertTrue(listIsSorted(zonesList));
+                driver.findElement(By.xpath("//button[@name='cancel']")).click();
+            }
+        }
+        return listIsSorted(countriesList);
+    }
+
+    private void countriesIsSortedOnGeoZonesPage() {
+        List<WebElement> countriesWebElementList = driver.findElements(By.xpath("//tr[@class='row']"));
+        for (int i = 0; i < countriesWebElementList.size(); i++) {
+            WebElement row = driver.findElement(By.xpath("//tr[@class='row'][" + (i + 1) + "]"));
+            row.findElement(By.xpath(".//td[3]/a")).click();
+            List<String> zonesList = new ArrayList<>();
+            List<WebElement> zonesWebElement = driver.findElements(By.xpath("//td[3]/select[starts-with(@name,'zones')]"));
+            for (WebElement webElementZone : zonesWebElement) {
+                zonesList.add(webElementZone.findElement(By.xpath(".//option[@selected='selected']")).getText());
+            }
+            Assert.assertTrue(listIsSorted(zonesList));
+            driver.findElement(By.xpath("//button[@name='cancel']")).click();
+        }
+    }
+
+    private boolean listIsSorted(List<String> list) {
+        List<String> sortableList = new ArrayList<>(list);
+        Collections.sort(sortableList);
+        return list.size() == sortableList.size() && list.containsAll(sortableList);
+    }
+
+    private void clickOnMenuByText(String text) {
+        driver.findElement(By.xpath(String.format("//*[contains(text(),'%s')]", text))).click();
     }
 }
